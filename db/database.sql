2452f9a6d2ef8d94fd1ed15856a8614fadc70e71
ericachia <ercachia@gmail.com>
2017-04-15 18:43:46 -0400

    created database

---
+++ db/database.sql
@@ -0,0 +1,16 @@
+CREATE TABLE file (
+	file_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	file_type VARCHAR(13),
+	file_name VARCHAR(255),
+	file_path VARCHAR(255),
+	date_added NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	file_data VARCHAR
+);
+
+CREATE TABLE keyword (
+	keyword_id INTEGER PRIMARY KEY AUTOINCREMENT,
+	file_id INTEGER,
+	keyword VARCHAR(255),
+	count INTEGER,
+	FOREIGN KEY (file_id) REFERENCES file(file_id)
+);
